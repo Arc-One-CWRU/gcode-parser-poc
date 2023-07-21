5f77abc7ebb2e727ed5f2eee498094c289d0e930
Michael Carlstrom <rmc@carlstrom.com>
2023-07-21 13:54:55 -0400

    added args

--- gcode_generator.py
+++ gcode_generator.py
@@ -10,16 +10,31 @@ SLEEP_START = "G4"
 
 ENABLE = "M42 P1 S1"
 DISABLE = "M42 P1 S0"
+NEWLINE = "\n"
 
 
 class GCODEGenerator:
 
     def __init__(self) -> None:
         parser = argparse.ArgumentParser()
-        parser.add_argument("-i", "--input_dir", help="Input directory of GCodes to transform.",
-                            type=str)
-        parser.add_argument("-o", "--output_dir", help="Output directory where the parsed GCodes will go.",
-                            type=str)
+        parser.add_argument("-w", "--weld_gap", help="Weld gap distance (mm).",
+                            type=float, default=8)
+        parser.add_argument("-wh", "--weld_layer_height", help="Weld height distance (mm).",
+                            type=float, default=2)
+        parser.add_argument("-ww", "--weld_layer_width", help="Weld width distance (mm).",
+                            type=float, default=2)
+        parser.add_argument("-ts", "--transport_speed", help="Set transport speed (mm).",
+                            type=float)
+        parser.add_argument("-ps", "--print_speed", help="Set print speed (mm).",
+                            type=float)
+        
+        parser.add_argument("-x", "--x_size", help="Set print speed (mm).",
+                            type=float)
+        parser.add_argument("-y", "--y_size", help="Set print speed (mm).",
+                            type=float)
+        parser.add_argument("-z", "--z_size", help="Set print speed (mm).",
+                            type=float)
+        
         parser.add_argument("-v", "--verbose", action="store_true",
                             help="Debug logging.")
         args = parser.parse_args()
@@ -27,11 +42,10 @@ class GCODEGenerator:
         print(args.input_dir)
 
     def run(self):
-
-        with open(os.path.join(DIR, f"{datetime.now()}.gcode").replace(" ", ""), "x") as f:
+        filename = f"{str(datetime.now())[:-7]}.gcode".replace(" ", " Time=").replace(":", " ")
+        with open(os.path.join(DIR, filename), "x") as f:
 
             f.write("test)")
-            f.write("pog)")
 
     def add_sleep(self, milliseconds: Optional[float] = None, seconds: Optional[int] = None):
         pass
