518dd0c1d9f43e24863b780390d69fde5fbcca8e
Joseph Chen <jchen42703@gmail.com>
2023-09-21 13:34:01 -0400

    feat(command): generalized G1ExtruderRemover to ExtruderRemover to take after the Micer

--- src/arcgcode/processsor/command/extruder_remover.py
+++ src/arcgcode/processsor/command/extruder_remover.py
@@ -2,8 +2,12 @@ import re
 from ..base.base import CommandProcessorInterface
 
 
-class G1ExtruderRemover(CommandProcessorInterface):
-    """Removes the extruder argument in G1 commands.
+class ExtruderRemover(CommandProcessorInterface):
+    """Removes the extruder argument in commands that have Xnnn, Ynnn, Znnn,
+    Ennn as arguments.
+
+    These commands include but are not limited to:
+    G0, G1, G2, G3, G92, M566
     """
 
     def __init__(self) -> None:
@@ -11,11 +15,27 @@ class G1ExtruderRemover(CommandProcessorInterface):
         self.extruder_g1_matcher = re.compile(
             r"([E][-+]?([0-9]*\.[0-9]*|[0-9]*))\w+")
 
+    def should_skip(self, gcode_instruction: str) -> bool:
+        """Checks if the given G-Code instruction should be skipped.
+
+        Args:
+            gcode_instruction (str): The G-Code instruction to check.
+
+        Returns:
+            bool: True if the instruction should be skipped.
+        """
+
+        skip_line = "X" not in gcode_instruction and \
+            "Y" not in gcode_instruction and \
+            "Z" not in gcode_instruction and " E" in gcode_instruction
+
+        return skip_line
+
     def process(self, gcode_instruction: str) -> str:
         """Matches a G-Code instruction. It should return
         the desired G-Code string for that line.
         """
-        if not gcode_instruction.startswith("G1"):
+        if self.should_skip(gcode_instruction):
             return gcode_instruction
 
         # Remove all extruder instructions in G1 commands
