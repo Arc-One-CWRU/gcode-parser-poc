f570e3336c40b0abfb2609cad7fe0e1765c61cd7
Joseph Chen <jchen42703@gmail.com>
2023-11-18 20:26:02 -0500

    fix(section): make AddGcodeVersion insert a comment instead of a raw string

--- src/arcgcode/processor/section/add_gcode_version.py
+++ src/arcgcode/processor/section/add_gcode_version.py
@@ -9,10 +9,11 @@ class AddGcodeVersion(SectionProcessorInterface):
         super().__init__()
 
     def process(self, gcode_section: list[str]) -> list[str]:
-        """Adds the git commit hash to the top of the G-Code files to differentiate versions
+        """Adds the git commit hash to the top of the G-Code files to
+        differentiate versions
         """
         git_hash = version.ARCGCODE_VERSION
-        gcode_version = f"Git Commit Hash (Version): {git_hash}\n"
+        gcode_version = f";Git Commit Hash (Version): {git_hash}\n"
         gcode_section.insert(0, gcode_version)
         return gcode_section
 
