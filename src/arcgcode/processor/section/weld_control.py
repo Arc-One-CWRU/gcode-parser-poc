28b198e7e3f84a97a25a103a4e262a21f298a8df
Joseph Chen <jchen42703@gmail.com>
2023-11-18 21:50:15 -0500

    fix(weld_control): add instruction when welder is turned off and added documentation

--- src/arcgcode/processor/section/weld_control.py
+++ src/arcgcode/processor/section/weld_control.py
@@ -10,42 +10,54 @@ class AllWelderControl(SectionProcessorInterface):
     def __init__(self) -> None:
         super().__init__()
         self.is_start_of_layer_matcher = re.compile(r';[A-Z]')
+    
+    def parse_extruder_cmd(self, cmd: str) -> str:
+        """Removes the extruder value the command.
+        """
+        e_index = cmd.find("E")
+        short_line = cmd[e_index:len(cmd)]
+        nums_in_line = re.findall(r"[-+]?(?:\d*\.*\d+)", short_line)
+        e_value = nums_in_line[0]
+        parsed_instruction = cmd.replace(f"E{e_value}", "")
+        return parsed_instruction
 
     def process(self, gcode_section: list[str]) -> list[str]:
         """Reads the G-Code file buffer and does an action. It should return
         the desired G-Code string for that section.
         """
         new_gcode_section: list[str] = []
-        seen_e = False
+        welder_is_on = False
         for i, instruction in enumerate(gcode_section):
             # Edge Cases
+            # Skip first line
             if i - 1 < 0:
                 new_gcode_section.append(instruction)
                 continue
+            # Skip last line
             if i + 1 == len(gcode_section):
                 new_gcode_section.append(instruction)
                 continue
-            if " E" in instruction and not seen_e:
-                seen_e = True
+
+            # Start the welder once when it's off and it encounters an
+            # instruction that requires it to extrude.
+            if " E" in instruction and not welder_is_on:
+                welder_is_on = True
                 new_gcode_section.append(GCodes.WELD_ON.value)
-                e_index = instruction.find("E")
-                short_line = instruction[e_index:len(instruction)]
-                nums_in_line = re.findall(r"[-+]?(?:\d*\.*\d+)", short_line)
-                e_value = nums_in_line[0]
-                parsed_instruction = instruction.replace(f"E{e_value}", "")
+                parsed_instruction = self.parse_extruder_cmd(instruction)
                 new_gcode_section.append(parsed_instruction)
-            elif " E" in instruction and seen_e:
-                # I'm not sure what this really does? XD
-                e_index = instruction.find("E")
-                short_line = instruction[e_index:len(instruction)]
-                nums_in_line = re.findall(r"[-+]?(?:\d*\.*\d+)", short_line)
-                e_value = nums_in_line[0]
-                parsed_instruction = instruction.replace(f"E{e_value}", "")
+            # If the welder is already on, extrude as usual
+            elif " E" in instruction and welder_is_on:
+                parsed_instruction = self.parse_extruder_cmd(instruction)
                 new_gcode_section.append(parsed_instruction)
-            elif " E" not in instruction and seen_e:
-                seen_e = False
+            # Turn the welder off the moment it encounters a non-extruding
+            # instruction.
+            elif " E" not in instruction and welder_is_on:
+                welder_is_on = False
                 new_gcode_section.append(GCodes.WELD_OFF.value)
-            elif " E" not in instruction and not seen_e:
+                new_gcode_section.append(instruction)
+            # Just append any instructions where the welder is off and there
+            # is no extruding.
+            elif " E" not in instruction and not welder_is_on:
                 new_gcode_section.append(instruction)
 
         return new_gcode_section
