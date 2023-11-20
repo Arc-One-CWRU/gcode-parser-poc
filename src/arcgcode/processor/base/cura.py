277c2eea88262b4a733b883468e5e2683d8e8d9a
Joseph Chen <jchen42703@gmail.com>
2023-11-19 21:44:07 -0500

    feat(processor): created find_end_of_gcode_movements_idx

--- src/arcgcode/processor/base/cura.py
+++ src/arcgcode/processor/base/cura.py
@@ -21,6 +21,27 @@ END_OF_GCODE_MOVEMENTS = ";TIME_ELAPSED"
 END_OF_GCODE = ";End of Gcode"
 
 
+def find_end_of_gcode_movements_idx(all_gcode_lines: list[str]) -> int:
+    """Gets the index of `all_gcode_lines` that represents the final line
+    of the GCODE_MOVEMENTS section. The END_OF_GCODE_MOVEMENTS constant is used
+    to match which line is the end.
+
+    Args:
+        all_gcode_lines: a list of all gcode instructions
+    Returns:
+        The index of the final line.
+    Raise:
+        Exception when there is no line that starts with END_OF_GCODE_MOVEMENTS
+    """
+    for i in range(len(all_gcode_lines)-1, -1, -1):
+        curr_line = all_gcode_lines[i]
+        if curr_line.strip().startswith(END_OF_GCODE_MOVEMENTS):
+            return i
+
+    raise Exception("could not find end of gcode movements where line " +
+                    f"starts with {END_OF_GCODE_MOVEMENTS}")
+
+
 def is_end_of_gcode_movements(gcode_line: str, curr_idx: int,
                               all_gcode_lines: list[str]) -> bool:
     """Checks to see if the current line is the end of the GCode movements
@@ -31,7 +52,7 @@ def is_end_of_gcode_movements(gcode_line: str, curr_idx: int,
         curr_idx: the index of all_gcode_lines where the current gcode_line is
         at
         all_gcode_lines: a list of all gcode instructions
-    """
+    """     
     at_boundary = gcode_line.startswith(END_OF_GCODE_MOVEMENTS)
     if not at_boundary:
         return False
@@ -52,7 +73,8 @@ def is_end_of_gcode_movements(gcode_line: str, curr_idx: int,
         # case.
         if (next_line.startswith(";TYPE") or
            next_line.startswith(";LAYER") or
-           next_line.startswith(";MESH")):
+           next_line.startswith(";MESH") or
+           next_line.strip() == ""):
             next_line_is_end_script = False
 
     return at_boundary and next_line_is_end_script
