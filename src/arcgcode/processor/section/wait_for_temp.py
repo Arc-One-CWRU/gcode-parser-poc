1780c531decf6c4891c5de4fec444a664aadcbf4
Michael Carlstrom <rmc@carlstrom.com>
2023-12-15 15:34:26 -0500

    Finished wait for temp

--- src/arcgcode/processor/section/wait_for_temp.py
+++ src/arcgcode/processor/section/wait_for_temp.py
@@ -19,13 +19,29 @@ class WaitForTemp(SectionProcessorInterface):
         new_gcode_section: list[str] = []
         skip_first = True
 
-        for _, instruction in enumerate(gcode_section):
+        for i, instruction in enumerate(gcode_section):
             if instruction.startswith(CURA_LAYER):
                 new_gcode_section.append(instruction)
                 if skip_first:
                     skip_first = False
                     continue
-                wait_instruction = f"{GCodes.WAIT.value} P2 S2\n"
+
+                num = 0
+                changed_movement = new_gcode_section[len(new_gcode_section)-3-num]
+                
+                x_index = changed_movement.find("X")
+                y_index = changed_movement.find("Y")
+                z_index = changed_movement.find("Z")
+                offset = 7.5
+                
+                new_x = str(float(changed_movement[x_index+1:y_index-1])+offset)
+                new_y = str(float(changed_movement[y_index+1:z_index-1])+offset)
+                
+                temp = f"{changed_movement[:x_index+1]}{new_x} Y{new_y} {changed_movement[z_index:]}"
+                print(f"before: {new_gcode_section[len(new_gcode_section)-3]}after: {temp}")
+                new_gcode_section[len(new_gcode_section)-3-num] = temp
+                
+                wait_instruction = f"{GCodes.WAIT.value} P2 S2"
                 new_gcode_section.append(wait_instruction)
             # Only care about the end of the movements section.
             # Assumed that it is Cura.
