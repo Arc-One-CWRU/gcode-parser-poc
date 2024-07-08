99986d15c5abcee0f01bd97f54fc4c2232b8bbdf
Henry Odza <hwodza@gmail.com>
2024-07-08 14:42:30 -0400

    Added init files in arcgcode tests, and filled out test_base abstract methods

---
+++ tests/arcgcode_tests/test_base/test_base.py
@@ -0,0 +1,44 @@
+from abc import ABCMeta, abstractmethod
+from arcgcode.processor.base.sections import GCodeSection
+
+
+class TestCommandProcessorInterface:
+    """Processes a matched G-Code instruction line.
+    """
+    __metaclass__ = ABCMeta
+
+    @abstractmethod
+    def process(self, gcode_instruction: str) -> str:
+        """Matches a G-Code instruction. It should return
+        a boolean dependent on if the line has the expected value from running
+        its matching Command Processor.
+        """
+        raise NotImplementedError
+
+
+class TestSectionProcessorInterface:
+    """A section processor should be applied on a "section", such as:
+    1. Top Comment (Settings & Metadata)
+    2. Startup Script
+    3. G-Code Movements
+        - Pre-Layer Action
+        - Movement
+        - Post-Layer Action
+    4. End Script
+    5. Bottom Comment
+    """
+    __metaclass__ = ABCMeta
+
+    @abstractmethod
+    def process(self, gcode_section: list[str]) -> list[str]:
+        """Reads the G-Code file with each command as an element and does an
+        action. It should return a boolean dependent on if the expected
+        commands are present from running its matching Section Processor.
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def section_type(self) -> GCodeSection:
+        """Returns the current section type.
+        """
+        raise NotImplementedError
