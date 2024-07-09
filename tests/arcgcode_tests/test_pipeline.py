4ea9eec6aeedfa4e81fdbc6fe61eb1f525647f90
Henry Odza <hwodza@gmail.com>
2024-07-09 13:54:19 -0400

    set up more boilerplate for testing pipeline

--- tests/arcgcode_tests/test_pipeline.py
+++ tests/arcgcode_tests/test_pipeline.py
@@ -1,3 +1,10 @@
+from .test_base.test_base import TestCommandProcessorInterface,  \
+    TestSectionProcessorInterface, GCodeSection
+
+
 class ArcGcodeTestPipeline(object):
     def __init__(self) -> None:
+        pass
+
+    def process(self) -> list[str]:
         pass
--- tests/arcgcode_tests/test_processor.py
+++ tests/arcgcode_tests/test_processor.py
@@ -1,9 +1,23 @@
 from arcgcode.cura.settings import CuraMicerSettings
+from test_pipeline import ArcGcodeTestPipeline
+from .test_base.test_base import TestCommandProcessorInterface,  \
+    TestSectionProcessorInterface
 
 
 class ArcGcodeTestProcessor():
     def __init__(self, settings: CuraMicerSettings) -> None:
         self.settings = settings
-    
+
     def execute(self) -> list[str]:
-        pass
+        section_tests_processors: list[TestSectionProcessorInterface] = [
+        ]
+
+        command_tests_processors: list[TestCommandProcessorInterface] = [
+        ]
+
+        test_pipeline = ArcGcodeTestPipeline(
+            section_tests_processors, 
+            command_tests_processors)
+
+        tests = test_pipeline.process()
+        return tests
