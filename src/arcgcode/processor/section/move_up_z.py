3d6ba45b0fad8c9d1bca55d5cc2b313f600e2f8b
Zachary Thomas <zet6@case.edu>
2024-07-15 12:35:09 -0400

    fixed comments in move_up_z.py and gcodes.py

--- src/arcgcode/processor/section/move_up_z.py
+++ src/arcgcode/processor/section/move_up_z.py
@@ -50,7 +50,7 @@ class MoveUpZ(SectionProcessorInterface):
             z_num = diff + self.get_z_num_from_instruction(instruction)
             z_front: str = instruction[0:z_index]
             new_z_instruction = z_front + str(z_num) + "\n"
-            new_gcode_section.append(new_z_instruction + "Added in move_up_z.py")
+            new_gcode_section.append(new_z_instruction + ";Added in move_up_z.py")
 
         return new_gcode_section
 
