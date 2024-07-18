a6b114534589d17bc9cd68ffcd76723e238513a2
Zachary Thomas <zet6@case.edu>
2024-07-18 16:40:49 -0400

    version change

--- src/arcgcode/processor/base/version.py
+++ src/arcgcode/processor/base/version.py
@@ -4,7 +4,7 @@ import re
 # Using commits for now since they are granular
 # In the future, could tag each arcgcode version with its release
 # and ditch this variable nonsense
-ARCGCODE_VERSION = "70948ed33de335babf003ef7a721e3caac8403f9"
+ARCGCODE_VERSION = "a24cdb37fa5e325d626b8effe55c731e2ad0853b"
 
 
 def get_current_arcgcode_version() -> str:
