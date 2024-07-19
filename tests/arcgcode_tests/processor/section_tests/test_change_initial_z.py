ca4e53b1808e03ff851877ff6d9d6f3efb187376
Zachary Thomas <zet6@case.edu>
2024-07-19 15:18:04 -0400

    partially completed test_change_initial_z.py

--- tests/arcgcode_tests/processor/section_tests/test_change_initial_z.py
+++ tests/arcgcode_tests/processor/section_tests/test_change_initial_z.py
@@ -1,10 +1,11 @@
 from ..test_base.test_base import TestSectionProcessorInterface, GCodeSection
-from arcgcode.processor.base import version
+from arcgcode.processor.base.cura import CURA_LAYER, CURA_TYPE_LAYER
 import unittest
 
 
 class TestChangeInitialZ(TestSectionProcessorInterface, unittest.TestCase):
-    """Adds the micer settings to GCode file.
+    """Tests that the initial z gets changed so that the welder doesn't run into clamps and
+    previous prints
     """
 
     def __init__(self) -> None:
@@ -16,18 +17,20 @@ class TestChangeInitialZ(TestSectionProcessorInterface, unittest.TestCase):
             self.gcode_section = gcode_section
 
         def test_change_initial_z(self):
-            flag = False
-            git_hash = version.ARCGCODE_VERSION
-            for instruction in self.gcode_section:
-                if git_hash in instruction:
-                    flag = True
-            
-            self.assertTrue(flag)
+            INITIAL_Z_MOVE = "G0 F9000 Z100"
+            flag1 = "Initial Z Offset comment was not added"
+            flag2 = "INITIAL_Z_MOVE was not added"
+            line = 1
+            while line > len(self.gcode_section):
+                if self.gcode_section[line] == ";Initial Z Offset Changed by change_initial_z.py":
+                    flag1 = "Initial Z Offset comment was added"
+                    if self.gcode_section[line+1] == INITIAL_Z_MOVE:
+                        flag2 = "INITIAL_Z_MOVE was added"
+                line += 1
+                self.assertEqual(flag1, "Initial Z Offset comment was added")
+                self.assertEqual(flag2, "INITIAL_Z_MOVE was added")
 
     def process(self, gcode_section: list[str]) -> list[str]:
-        """Adds the git commit hash to the top of the G-Code files to
-        differentiate versions
-        """
         self.gcode_section = gcode_section
         tests = [self.Test("test_change_initial_z", gcode_section)]
         return unittest.TestSuite(tests=tests)
