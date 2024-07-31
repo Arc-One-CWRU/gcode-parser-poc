d14fcc4dc764cc1c698b30c8b8e4c060106af54c
Zachary Thomas <zet6@case.edu>
2024-07-31 09:34:38 -0400

    test edits and test_processor edits

--- tests/arcgcode_tests/processor/section_tests/test_add_sleep.py
+++ tests/arcgcode_tests/processor/section_tests/test_add_sleep.py
@@ -21,23 +21,22 @@ class TestAddSleep(TestSectionProcessorInterface, unittest.TestCase):
             flag1 = "Sleep was not added"
             flag2 = "Sleep amount is incorrect"
             flag3 = "add_sleep comment was not added"
-            if ";Added in wait_for_temp.py" not in self.gcode_section:
-                (ds, s) = modf(self.sleep_time)
-                # Converts into ms
-                ms = int(ds*1000)
-                line = 1
-                while line < len(self.gcode_section):
-                    if ";LAYER:" in self.gcode_section[line] and "0" not in self.gcode_section[line][7]:
-                        if "G4 S" in self.gcode_section[line+1]:
-                            flag1 = "Sleep was added"
-                        if str(int(s)) in self.gcode_section[line+1] and str(ms) in self.gcode_section[line+1]:
-                            flag2 = "Sleep amount is correct"
-                        if self.gcode_section[line+2] == ";Added sleep in add_sleep.py\n":
-                            flag3 = "add_sleep comment was added"
-                    line += 1
-                self.assertEqual(flag1, "Sleep was added")
-                self.assertEqual(flag2, "Sleep amount is correct")
-                self.assertEqual(flag3, "add_sleep comment was added")
+            (ds, s) = modf(self.sleep_time)
+            # Converts into ms
+            ms = int(ds*1000)
+            line = 1
+            while line < len(self.gcode_section):
+                if ";LAYER:" in self.gcode_section[line] and "0" not in self.gcode_section[line][7]:
+                    if "G4 S" in self.gcode_section[line+1]:
+                        flag1 = "Sleep was added"
+                    if str(int(s)) in self.gcode_section[line+1] and str(ms) in self.gcode_section[line+1]:
+                        flag2 = "Sleep amount is correct"
+                    if self.gcode_section[line+2] == ";Added sleep in add_sleep.py\n":
+                        flag3 = "add_sleep comment was added"
+                line += 1
+            self.assertEqual(flag1, "Sleep was added")
+            self.assertEqual(flag2, "Sleep amount is correct")
+            self.assertEqual(flag3, "add_sleep comment was added")
 
     def process(self, gcode_section: list[str]) -> list[str]:
         """Reads the G-Code file buffer and does an action. It should return
